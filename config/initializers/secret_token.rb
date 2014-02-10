537cd6faeaa0eaa6ad95616d6a7f8550f4178ccc
Anirvan Mandal <anirvan@hackerrank.com>
2014-02-11 00:02:24 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LinkUp::Application.config.secret_key_base = '824d152731cc1860fbd2400d0252193db799578f6365d32f8eacdb0668701687378adde9845afc5d9d4132f1f3993672637a333363205873b432d7c7b308e87e'
